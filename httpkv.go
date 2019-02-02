a7df4ba73a9f3029a19f046cab19a1f7b9a219bf
Grégoire Piffault <greg.piffault@gmail.com>
2019-02-02 13:39:16 +0100

    Refactor with a net.http.Handler

--- httpkv.go
+++ httpkv.go
@@ -13,37 +13,11 @@ func main() {
 	db, err := sql.Open("sqlite3", "data.db")
 	checkErr(err)
 	defer db.Close()
-	stmt, err := db.Prepare("create table if not exists data (key BLOB PRIMARY KEY, value BLOB)")
+	_, err = db.Exec("create table if not exists data (key BLOB PRIMARY KEY, value BLOB)")
 	checkErr(err)
-	_, err = stmt.Exec()
-	checkErr(err)
-	stmt.Close()
 
-	http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
-		switch r.Method {
-		case http.MethodGet:
-			rows, err := db.Query("select value from data where key = ?", r.URL.Path)
-			checkErr(err)
-			defer rows.Close()
-			if !rows.Next() {
-				w.WriteHeader(http.StatusNotFound)
-				return
-			}
-			var value string
-			rows.Scan(&value)
-			fmt.Fprintf(w, value)
-		case http.MethodPut:
-			body, err := ioutil.ReadAll(r.Body)
-			checkErr(err)
-			stmt, err = db.Prepare("insert into data values (?,?)")
-			checkErr(err)
-			defer stmt.Close()
-			_, err = stmt.Exec(r.URL.Path, body)
-			checkErr(err)
-		default:
-			w.WriteHeader(http.StatusNotImplemented)
-		}
-	})
+	handler := &KVHandler{db: db}
+	http.Handle("/", handler)
 
 	err = http.ListenAndServe(":8765", nil)
 	log.Fatal(err)
@@ -53,4 +27,39 @@ func checkErr(err error) {
 	if err != nil {
 		panic(err)
 	}
+}
+
+type KVHandler struct {
+	db *sql.DB
+}
+
+func (handler KVHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		handler.Get(w, r)
+	case http.MethodPut:
+		handler.Put(w, r)
+	default:
+		w.WriteHeader(http.StatusMethodNotAllowed)
+	}
+}
+
+func (handler KVHandler) Get(w http.ResponseWriter, r *http.Request) {
+	rows, err := handler.db.Query("select value from data where key = ?", r.URL.Path)
+	checkErr(err)
+	defer rows.Close()
+	if !rows.Next() {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+	var value string
+	rows.Scan(&value)
+	fmt.Fprintf(w, value)
+}
+
+func (handler KVHandler) Put(w http.ResponseWriter, r *http.Request) {
+	body, err := ioutil.ReadAll(r.Body)
+	checkErr(err)
+	_, err = handler.db.Exec("insert or replace into data values (?,?)", r.URL.Path, body)
+	checkErr(err)
 }
